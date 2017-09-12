3681a6bdf055ffc12e1a026d08c0f9e7baa9e2cb
Shivawkes <londondtm23@gmail.com>
2017-09-12 10:07:12 -0400

    Update PublicAudio.php

--- PublicAudio.php
+++ PublicAudio.php
@@ -13,8 +13,8 @@ class PublicAudioPlugin extends Plugin
   function onPluginVersion(array &$versions)
     {
       $versions[] = array('name' => 'PublicAudio',
-                          'author' => 'London Longenecker',
-                          'rawdescription' => _m('A plugin to play mp3 files when viewing the login page.'));
+                          'author' => 'HeathCliff',
+                          'rawdescription' => _m('A plugin to play mp3 files when viewing the timeline on the login page.'));
       return true;
     }
 }
