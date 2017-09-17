047a2f3c035fdfaf7552da7145cccf1496732aa9
Shivawkes <22037550+Shivawkes@users.noreply.github.com>
2017-09-17 12:34:18 -0400

    Update playTrack.js

--- js/playTrack.js
+++ js/playTrack.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   if(window.loggedIn === false) {
     
     var trackPath = '../audio/',
-        audioFiles = ['.mp3', '.mp3', '.mp3'],
+        audioFiles = ['LeVrai.mp3', 'Stars.mp3', '.mp3'],
         i = Math.floor(Math.random()*audioFiles.length);
         
     var audio = new Audio(trackPath+audioFiles[i]);
