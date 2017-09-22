558005250377627014ffb335ddb2fb4bfacc4e0d
Shivawkes <22037550+Shivawkes@users.noreply.github.com>
2017-09-22 12:28:12 -0400

    Update playTrack.js

--- js/playTrack.js
+++ js/playTrack.js
@@ -1,17 +1,25 @@
 $(document).ready(function() {
-  
-  if(window.loggedIn === false) {
-    
-    var trackPath = '../audio/',
-        audioFiles = ['LeVrai.mp3', 'Stars.mp3', 'FireDarer.mp3'],
-        i = Math.floor(Math.random()*audioFiles.length);
-        
-    var audio = new Audio(trackPath+audioFiles[i]);
-    audio.play();
-    
-  } else {
-    
-    audio.pause();
-    
-  }
-});
+
+// only play music on home page while not logged in
+if(window.loggedIn === false) {
+
+// randomly select a track from the array
+var path = '../audio/',
+files = ['LeVrai.mp3', 'FireDarer.mp3', 'WhatNow.mp3'],
+i = Math.floor(Math.random()*files.length);
+
+// play the track
+var audio = new Audio(path+files[i]);
+audio.play();
+
+// repeat the track
+audio.addEventListener('ended', function() {
+this.currentTime = 0;
+this.play();
+
+}, false);
+
+
+} //END WINDOW LOGGEDIN
+
+}); //END DOCREADY
